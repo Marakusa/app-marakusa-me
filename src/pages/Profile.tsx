e6f55523e2ea9e4e9b1266e6ef9df142e5ff444d
Markus Kannisto <m.kannisto01@gmail.com>
2025-04-24 13:39:26 +0300

    Fix profile crashing when reloading at /profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -17,21 +17,25 @@ function Profile() {
     return (
         <>
             <title>Naali - Profile</title>
-        
+
             <div className="flex flex-col items-center justify-center h-screen">
-                <h3 className="text-4xl font-bold mb-4">Hello, {(profile.data as any).username}!</h3>
-                {profile.fetched ? profile.data ? (
+                {profile.fetched && profile.data && (
                     <>
-                        <img src={
-                            (profile.data as any).avatar.startsWith("https://") ? 
-                            (profile.data as any).avatar : 
-                            `https://cdn.discordapp.com/avatars/${(profile.data as any).discord_id}/${(profile.data as any).avatar}.png`} alt="Avatar" className="w-32 h-32 rounded-full mb-4" />
-                        <p className="text-lg">{(profile.data as any).username}</p>
+                        <h3 className="text-4xl font-bold mb-4">Hello, {(profile.data as any).username}!</h3>
+                        {profile.fetched ? profile.data ? (
+                            <>
+                                <img src={
+                                    (profile.data as any).avatar.startsWith("https://") ?
+                                        (profile.data as any).avatar :
+                                        `https://cdn.discordapp.com/avatars/${(profile.data as any).discord_id}/${(profile.data as any).avatar}.png`} alt="Avatar" className="w-32 h-32 rounded-full mb-4" />
+                                <p className="text-lg">{(profile.data as any).username}</p>
+                            </>
+                        ) : (
+                            <p className="text-lg">No profile data available.</p>
+                        ) : (
+                            <p className="text-lg">Loading...</p>
+                        )}
                     </>
-                ) : (
-                    <p className="text-lg">No profile data available.</p>
-                ) : (
-                    <p className="text-lg">Loading...</p>
                 )}
             </div>
         </>
