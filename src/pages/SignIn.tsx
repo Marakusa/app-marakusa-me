5367698aed9fbdfd10e207db1516f40b20ebdc91
Marakusa <m.kannisto01@gmail.com>
2026-08-03 19:39:33 +0300

    Add privacy disclaimer for sign in

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -33,6 +33,11 @@ function SignIn() {
             <div className="flex flex-col gap-8 justify-center items-center w-10/12 max-w-150 bg-zinc-900 border border-zinc-800 rounded-3xl shadow-lg shadow-black/20 p-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <div className="text-3xl">Sign In</div>
                 <p>If you want to access all of the files, you have to connect your Discord account to link your avatar licenses.</p>
+
+                <div className="flex flex-col gap-2 justify-center items-center bg-blue-300/30 font-bold rounded-3xl shadow-lg shadow-black/20 border border-blue-300 p-4 text-left">
+                    ℹ️ We do not collect any passwords or personal information other than your Discord user ID, username, and avatar picture. Your data is only used to verify your licenses and will not be shared with any third parties.
+                </div>
+
                 <p>Your licenses here are synced with <a href="https://discord.com/invite/47SrTE3Spw" target="_blank" className="text-blue-400">Mara's Frosty Lodge</a> Discord server.</p>
                 <div className="flex flex-row gap-4">
                     <a
